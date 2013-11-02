4eea35391b0b14fa4a062053761a2302a957d3a3
Tim Greeno <timgreeno@berkeley.edu>
2013-11-01 22:05:22 -0700

    3

--- logic.py
+++ logic.py
@@ -1,19 +1,34 @@
 from random import randint
 
-
 options = {2 : 'rock', 3 : 'paper', 4 : 'scissors'}
-
-
 win = 1
 loss = 0
 tie = 'tie'
 
 def game(player_choice):
-		def rock_chosen(opponent):
-			if opponent
-				return tie
-			else if 
 
+    def rock_chosen(opponent):
+        if opponent == 'rock':
+            return tie
+        elif opponent == 'scissors':
+            return win
+        return loss
 
-		cpu_choice = randint(range(2,5))
-		if player == 
+    def paper_chosen(opponent):
+        if opponent == 'paper':
+            return tie
+        elif opponent == 'rock':
+            return win
+        return loss
+
+    def scissors_chosen(opponent):
+        if opponent == 'scissors':
+            return tie
+        elif opponent == 'paper':
+            return win
+        return loss
+        
+    functions = {'rock':rock_chosen, 'paper':paper_chosen,
+     'scissors':scissors_chosen}
+    cpu_choice = options[randint(2,4)]
+    return functions[player_choice](cpu_choice)
