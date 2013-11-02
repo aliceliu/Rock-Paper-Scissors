eb8a27f94e962ca71b5ca9b8c89d51992d422f69
Tim Greeno <timgreeno@berkeley.edu>
2013-11-01 21:41:03 -0700

    added logic file to work on with alberto (not nearly done)

--- logic.py
+++ logic.py
@@ -1,2 +1,15 @@
-def game(play):
-	
+from random import randint
+
+options = {'rock' : 0, 'paper': 1, 'scissors' : 2}
+win = 1
+loss = 0
+tie = 'tie'
+
+def game(selection):
+		def rocks(opponent):
+			if opponent == 0:
+				return tie
+
+		player_choice = options[selection]
+		cpu_choice = randint(range(3))
+		if player == 0
