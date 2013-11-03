86b6e4b871c37867d107b831bb823a7c3a1bf0f3
Tim Greeno <timgreeno@berkeley.edu>
2013-11-02 18:37:54 -0700

    changed up format, much better

--- logic.py
+++ logic.py
@@ -1,39 +1,27 @@
 from random import randint
 
-options = {0 : 'rock', 1 : 'paper', 2 : 'scissors'}
+cpu_options = {0 : 'rock', 1 : 'paper', 2 : 'scissors'}
 win = 'Congratulations, you won!'
 loss = 'Sorry, but you lost this time.'
 tie = "It's a tie, play again!"
 
+results = {('rock', 'rock'): tie,
+           ('rock', 'scissors'): win,
+           ('rock', 'paper'): loss,
+           ('scissors', 'scissors'): tie,
+           ('scissors', 'paper'): win,
+           ('scissors', 'rock'): loss,
+           ('paper', 'paper'): tie,
+           ('paper', 'rock'): win,
+           ('paper', 'scissors'): loss
+                    }
+
+def get_random_choice():
+    return cpu_options[randint(0,2)]
+
 def play(player_choice, opponent):
-    return functions[player_choice](opponent)
+    return results[(player_choice, opponent)]
 
-########### Game Logic #############
 
-def get_cpu_choice():
-    return options[randint(0,2)]
 
-def rock_chosen(opponent):
-    if opponent == 'rock':
-        return tie
-    elif opponent == 'scissors':
-        return win
-    return loss
 
-def paper_chosen(opponent):
-    if opponent == 'paper':
-        return tie
-    elif opponent == 'rock':
-        return win
-    return loss
-
-def scissors_chosen(opponent):
-    if opponent == 'scissors':
-        return tie
-    elif opponent == 'paper':
-        return win
-    return loss
-    
-functions = {'rock': rock_chosen, 
-             'paper': paper_chosen, 
-             'scissors': scissors_chosen}
